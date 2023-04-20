6c04eb2729ce5741d3e9053364ba9d5532fc203b
Ricardo <123037383+RichardoTS@users.noreply.github.com>
2023-04-20 15:48:24 +0000

    routs

--- src/app.py
+++ src/app.py
@@ -2,6 +2,7 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 import os
+import uuid
 from flask import Flask, request, jsonify, url_for, send_from_directory
 from flask_migrate import Migrate
 from flask_swagger import swagger
@@ -76,3 +77,48 @@ def serve_any_other_file(path):
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3001))
     app.run(host='0.0.0.0', port=PORT, debug=True)
+
+stores = []
+items = []
+
+@app.get("/store")
+def get_stores():
+    return {"stores": list(stores.values())}
+
+@app.post("/store")
+def create_store():
+    store_data = request.data_json()
+    store_id = uuid.uuid4().hex
+    store = {**store_data, "id": store_id}
+    stores[store_id] = store
+    return store, 201
+
+@app.post("/item")
+def create_item():
+    item_data = request.data_json()
+    if item_data["store_id"] not in stores:
+        return {"message": "Store not found"}, 404
+
+    item_id = uuid.uuid4().hex
+    item = {**item_data, "id": item_id}
+    items[item_id] = item
+    
+    return item, 201
+
+@app.get("/item")
+def get_all_items():
+    return {"items": list(items.values())}
+
+@app.get("/store/<string:store_id>")
+def get_store(store_id):
+    try:
+        return store[stodes_id]
+    except KeyError:
+        return {"message": "Store not found"}, 404
+
+@app.get("/item/<string:item_id>")
+def get_item(item_id):
+    try:
+        return items[item_id]
+    except KeyError:
+        return {"message": "Item not found"}, 404
