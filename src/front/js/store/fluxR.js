a7f6453607ceed64b3a1bc01ea8b1b8786db6f61
E-Vivanco <vivanco.elsa@gmail.com>
2023-04-27 02:06:47 +0000

    nuevo ajuste en store de flux

--- src/front/js/store/fluxR.js
+++ src/front/js/store/fluxR.js
@@ -1,6 +1,9 @@
 const getState = ({ getStore, getActions, setStore }) => {
   return {
-    store: {},
+    store: {
+      RUTA_FLASK_API:
+        "https://3001-evivanco-proyectofinalp-0j2ufapbd8a.ws-eu95.gitpod.io",
+    },
     actions: {
       // Use getActions to call a function within a fuction
     },
