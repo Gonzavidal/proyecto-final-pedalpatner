048b8fc40979888136b1d3117ab377871e9c9bc6
E-Vivanco <vivanco.elsa@gmail.com>
2023-05-03 20:37:49 +0000

    validacion en registro de ciclista + mecanico

--- src/front/js/pages/registro.jsx
+++ src/front/js/pages/registro.jsx
@@ -1,9 +1,13 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import { Context } from "../store/AppContext";
+import { useNavigate } from "react-router-dom";
 
 export const Registro = () => {
   const { store, actions } = useContext(Context);
-  const [isChecked, setIsChecked] = useState(false);
+  const navigate = useNavigate();
+  // const [isChecked, setIsChecked] = useState(false);
+  //const[inputvalor, setinputvalor] = useState(0);
+
   return (
     <div
       className="row container-fluid position-relative text-center p-3"
@@ -13,9 +17,14 @@ export const Registro = () => {
       <hr className="col-12 hr my-2" />
 
       {/* form container 4 whole body */}
+
       <form
         className="col-12 mt-1 px-5 position-relative"
-        onSubmit={actions.handleSubmitContacto}
+        onSubmit={(e) =>
+          store.roles_id == 2
+            ? actions.handleSubmitRegister(e, navigate)
+            : actions.handleSubmitRegisterM(e, navigate)
+        }
       >
         <div className="row container-fluid justify-content-center">
           <h3 className="col-12">¿Qué tipo de usuario eres?</h3>
@@ -24,10 +33,11 @@ export const Registro = () => {
               <input
                 className="form-check-input"
                 type="radio"
-                name="tipos_id"
+                name="roles_id"
                 id="flexRadioDefault1"
-                value={store.tipos_id}
+                value={2}
                 onChange={actions.handleChange}
+                setinputvalor={store.value}
               />
               <label className="form-check-label" htmlFor="flexRadioDefault1">
                 Ciclista
@@ -37,11 +47,12 @@ export const Registro = () => {
               <input
                 className="form-check-input"
                 type="radio"
-                name="tipos_id"
+                name="roles_id"
                 id="flexRadioDefault2"
-                checked={isChecked}
-                value={store.tipos_id}
+                //checked={isChecked}
+                value={3}
                 onChange={actions.handleChange}
+                setinputvalor={store.value}
               />
               <label
                 className="form-check-label fs-5"
@@ -52,53 +63,178 @@ export const Registro = () => {
             </div>
           </div>
         </div>
-        <div className="row d-inline justify-content-center mb-3 fs-5">
-          <div className="col-7 my-2 form-floating d-flex mx-auto">
-            <input
-              type="text"
-              className="form-control"
-              name="username"
-              id="inputUsername"
-              aria-label="searchInput"
-              aria-describedby="inputGroup"
-              value={store.username}
-              onChange={actions.handleChange}
-            />
-            <label className="pt-2 mx-2" htmlFor="#inputUsername">
-              Username
-            </label>
+        {store.roles_id == 2 ? (
+          <div className="row d-inline justify-content-center mb-3 fs-5">
+            <div className="col-7 my-2 form-floating d-flex mx-auto">
+              <input
+                type="text"
+                className="form-control"
+                name="username"
+                id="inputUsername"
+                aria-label="searchInput"
+                aria-describedby="inputGroup"
+                value={store.username}
+                onChange={actions.handleChange}
+              />
+              <label className="pt-2 mx-2" htmlFor="#inputUsername">
+                Username
+              </label>
+            </div>
+            <div className="col-7 my-2 form-floating d-flex mx-auto">
+              <input
+                type="text"
+                className="form-control"
+                name="email"
+                id="inputEmail"
+                aria-label="searchInput"
+                aria-describedby="inputGroup"
+                value={store.email}
+                onChange={actions.handleChange}
+              />
+              <label className="pt-2 mx-2" htmlFor="#inputEmail">
+                Email
+              </label>
+            </div>
+            <div className="col-7 my-2 form-floating d-flex mx-auto">
+              <input
+                type="password"
+                className="form-control"
+                name="password"
+                id="inputPassword"
+                aria-label="passwordInput"
+                aria-describedby="inputGroup"
+                value={store.password}
+                onChange={actions.handleChange}
+              />
+              <label className="pt-2 mx-2" htmlFor="#inputPassword">
+                Password
+              </label>
+            </div>
+            <div className="col-7 my-2 form-floating d-flex mx-auto">
+              <input
+                type="text"
+                className="form-control"
+                name="direccion"
+                id="inputDireccion"
+                aria-label="textdInput"
+                aria-describedby="inputGroup"
+                value={store.direccion}
+                onChange={actions.handleChange}
+              />
+              <label className="pt-2 mx-2" htmlFor="#inputDireccion">
+                Direccion
+              </label>
+            </div>
           </div>
-          <div className="col-7 my-2 form-floating d-flex mx-auto">
-            <input
-              type="text"
-              className="form-control"
-              name="email"
-              id="inputEmail"
-              aria-label="searchInput"
-              aria-describedby="inputGroup"
-              value={store.email}
-              onChange={actions.handleChange}
-            />
-            <label className="pt-2 mx-2" htmlFor="#inputEmail">
-              Email
-            </label>
+        ) : store.roles_id == 3 ? (
+          <div className="row d-inline justify-content-center mb-3 fs-5">
+            <div className="col-7 my-2 form-floating d-flex mx-auto">
+              <input
+                type="text"
+                className="form-control"
+                name="username"
+                id="inputUsername"
+                aria-label="searchInput"
+                aria-describedby="inputGroup"
+                value={store.username}
+                onChange={actions.handleChange}
+              />
+              <label className="pt-2 mx-2" htmlFor="#inputUsername">
+                Username
+              </label>
+            </div>
+            <div className="col-7 my-2 form-floating d-flex mx-auto">
+              <input
+                type="text"
+                className="form-control"
+                name="email"
+                id="inputEmail"
+                aria-label="searchInput"
+                aria-describedby="inputGroup"
+                value={store.email}
+                onChange={actions.handleChange}
+              />
+              <label className="pt-2 mx-2" htmlFor="#inputEmail">
+                Email
+              </label>
+            </div>
+            <div className="col-7 my-2 form-floating d-flex mx-auto">
+              <input
+                type="password"
+                className="form-control"
+                name="password"
+                id="inputPassword"
+                aria-label="passwordInput"
+                aria-describedby="inputGroup"
+                value={store.password}
+                onChange={actions.handleChange}
+              />
+              <label className="pt-2 mx-2" htmlFor="#inputPassword">
+                Password
+              </label>
+            </div>
+            <div className="col-7 my-2 form-floating d-flex mx-auto">
+              <input
+                type="text"
+                className="form-control"
+                name="direccion"
+                id="inputDireccion"
+                aria-label="textdInput"
+                aria-describedby="inputGroup"
+                value={store.direccion}
+                onChange={actions.handleChange}
+              />
+              <label className="pt-2 mx-2" htmlFor="#inputDireccion">
+                Direccion
+              </label>
+            </div>
+            <div className="col-7 my-2 form-floating d-flex mx-auto">
+              <input
+                type="text"
+                className="form-control"
+                name="tallernom"
+                id="inputTallernom"
+                aria-label="textdInput"
+                aria-describedby="inputGroup"
+                value={store.tallernom}
+                onChange={actions.handleChange}
+              />
+              <label className="pt-2 mx-2" htmlFor="#inputTallernom">
+                Nombre Taller
+              </label>
+            </div>
+            <div className="col-7 my-2 form-floating d-flex mx-auto">
+              <input
+                type="text"
+                className="form-control"
+                name="regiontall"
+                id="inputRegiontall"
+                aria-label="textdInput"
+                aria-describedby="inputGroup"
+                value={store.regiontall}
+                onChange={actions.handleChange}
+              />
+              <label className="pt-2 mx-2" htmlFor="#inputRegiontall">
+                Region Taller
+              </label>
+            </div>
+            <div className="col-7 my-2 form-floating d-flex mx-auto">
+              <input
+                type="text"
+                className="form-control"
+                name="direcciontall"
+                id="inputDirecciontall"
+                aria-label="textdInput"
+                aria-describedby="inputGroup"
+                value={store.direcciontall}
+                onChange={actions.handleChange}
+              />
+              <label className="pt-2 mx-2" htmlFor="#inputDirecciontall">
+                Direccion Taller
+              </label>
+            </div>
           </div>
-          <div className="col-7 my-2 form-floating d-flex mx-auto">
-            <input
-              type="password"
-              className="form-control"
-              name="password"
-              id="inputPassword"
-              aria-label="passwordInput"
-              aria-describedby="inputGroup"
-              value={store.password}
-              onChange={actions.handleChange}
-            />
-            <label className="pt-2 mx-2" htmlFor="#inputPassword">
-              Password
-            </label>
-          </div>
-        </div>
+        ) : null}
         <div className="row mt-3">
           <button
             type="submit"
