4c3fc738ce43cbbbbfb5703e37206ffb9e74c8f5
E-Vivanco <vivanco.elsa@gmail.com>
2023-05-04 21:10:40 +0000

    new BD pedatpartner_3

--- migrations/versions/600a78e619c3_.py
+++ migrations/versions/600a78e619c3_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 6ccaf63ec007
+Revision ID: 600a78e619c3
 Revises: 
-Create Date: 2023-04-24 13:36:51.969214
+Create Date: 2023-05-04 21:09:06.511240
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '6ccaf63ec007'
+revision = '600a78e619c3'
 down_revision = None
 branch_labels = None
 depends_on = None
@@ -23,9 +23,9 @@ def upgrade():
     sa.Column('created_at', sa.DateTime(), nullable=True),
     sa.Column('updated_at', sa.DateTime(), nullable=True),
     sa.Column('articulonom', sa.String(length=200), nullable=False),
-    sa.Column('precio', sa.Integer(), nullable=False),
-    sa.Column('promocion', sa.Boolean(), nullable=False),
-    sa.Column('precio_oferta', sa.Integer(), nullable=False),
+    sa.Column('mantencion', sa.Integer(), nullable=True),
+    sa.Column('indumentaria', sa.Integer(), nullable=True),
+    sa.Column('bicicletas', sa.Integer(), nullable=True),
     sa.PrimaryKeyConstraint('id')
     )
     op.create_table('pagos',
@@ -43,6 +43,15 @@ def upgrade():
     sa.Column('tiporol', sa.String(length=100), nullable=False),
     sa.PrimaryKeyConstraint('id')
     )
+    op.create_table('talleres',
+    sa.Column('id', sa.Integer(), nullable=False),
+    sa.Column('created_at', sa.DateTime(), nullable=True),
+    sa.Column('updated_at', sa.DateTime(), nullable=True),
+    sa.Column('tallernom', sa.String(length=110), nullable=True),
+    sa.Column('regiontall', sa.String(length=110), nullable=True),
+    sa.Column('direcciontall', sa.String(length=250), nullable=True),
+    sa.PrimaryKeyConstraint('id')
+    )
     op.create_table('tipos',
     sa.Column('id', sa.Integer(), nullable=False),
     sa.Column('created_at', sa.DateTime(), nullable=True),
@@ -50,46 +59,6 @@ def upgrade():
     sa.Column('nombre', sa.String(length=100), nullable=False),
     sa.PrimaryKeyConstraint('id'),
     sa.UniqueConstraint('nombre')
-    )
-    op.create_table('users',
-    sa.Column('id', sa.Integer(), nullable=False),
-    sa.Column('created_at', sa.DateTime(), nullable=True),
-    sa.Column('updated_at', sa.DateTime(), nullable=True),
-    sa.Column('username', sa.String(length=120), nullable=False),
-    sa.Column('email', sa.String(length=120), nullable=False),
-    sa.Column('password', sa.String(length=390), nullable=False),
-    sa.Column('direccion', sa.String(length=220), nullable=False),
-    sa.Column('is_active', sa.Boolean(), nullable=False),
-    sa.Column('roles_id', sa.Integer(), nullable=False),
-    sa.ForeignKeyConstraint(['roles_id'], ['roles.id'], ),
-    sa.PrimaryKeyConstraint('id'),
-    sa.UniqueConstraint('email'),
-    sa.UniqueConstraint('username')
-    )
-    op.create_table('comunicacion',
-    sa.Column('id', sa.Integer(), nullable=False),
-    sa.Column('titulo', sa.String(length=130), nullable=False),
-    sa.Column('email', sa.String(length=100), nullable=False),
-    sa.Column('descripcion', sa.Text(), nullable=False),
-    sa.Column('destino', sa.Integer(), nullable=False),
-    sa.Column('created_at', sa.DateTime(), nullable=True),
-    sa.Column('updated_at', sa.DateTime(), nullable=True),
-    sa.Column('tipos_id', sa.Integer(), nullable=True),
-    sa.Column('users_id', sa.Integer(), nullable=True),
-    sa.ForeignKeyConstraint(['tipos_id'], ['tipos.id'], ),
-    sa.ForeignKeyConstraint(['users_id'], ['users.id'], ),
-    sa.PrimaryKeyConstraint('id')
-    )
-    op.create_table('talleres',
-    sa.Column('id', sa.Integer(), nullable=False),
-    sa.Column('tallernom', sa.String(length=100), nullable=True),
-    sa.Column('regiontall', sa.String(length=100), nullable=True),
-    sa.Column('direcciontall', sa.String(length=250), nullable=True),
-    sa.Column('users_id', sa.Integer(), nullable=True),
-    sa.Column('created_at', sa.DateTime(), nullable=True),
-    sa.Column('updated_at', sa.DateTime(), nullable=True),
-    sa.ForeignKeyConstraint(['users_id'], ['users.id'], ),
-    sa.PrimaryKeyConstraint('id')
     )
     op.create_table('pagotalleres',
     sa.Column('pagos_id', sa.Integer(), nullable=False),
@@ -109,6 +78,34 @@ def upgrade():
     sa.ForeignKeyConstraint(['talleres_id'], ['talleres.id'], ),
     sa.PrimaryKeyConstraint('talleres_id', 'articulos_id')
     )
+    op.create_table('users',
+    sa.Column('id', sa.Integer(), nullable=False),
+    sa.Column('created_at', sa.DateTime(), nullable=True),
+    sa.Column('updated_at', sa.DateTime(), nullable=True),
+    sa.Column('username', sa.String(length=120), nullable=False),
+    sa.Column('email', sa.String(length=120), nullable=False),
+    sa.Column('password', sa.String(length=400), nullable=False),
+    sa.Column('is_active', sa.Boolean(), nullable=False),
+    sa.Column('roles_id', sa.Integer(), nullable=False),
+    sa.ForeignKeyConstraint(['roles_id'], ['roles.id'], ),
+    sa.PrimaryKeyConstraint('id'),
+    sa.UniqueConstraint('email'),
+    sa.UniqueConstraint('username')
+    )
+    op.create_table('comunicacion',
+    sa.Column('id', sa.Integer(), nullable=False),
+    sa.Column('titulo', sa.String(length=140), nullable=False),
+    sa.Column('email', sa.String(length=110), nullable=False),
+    sa.Column('descripcion', sa.Text(), nullable=False),
+    sa.Column('created_at', sa.DateTime(), nullable=True),
+    sa.Column('updated_at', sa.DateTime(), nullable=True),
+    sa.Column('tipos_id', sa.Integer(), nullable=False),
+    sa.Column('files', sa.LargeBinary(), nullable=True),
+    sa.Column('users_id', sa.Integer(), nullable=True),
+    sa.ForeignKeyConstraint(['tipos_id'], ['tipos.id'], ),
+    sa.ForeignKeyConstraint(['users_id'], ['users.id'], ),
+    sa.PrimaryKeyConstraint('id')
+    )
     op.create_table('usertalleres',
     sa.Column('talleres_id', sa.Integer(), nullable=False),
     sa.Column('users_id', sa.Integer(), nullable=False),
@@ -124,12 +121,12 @@ def upgrade():
 def downgrade():
     # ### commands auto generated by Alembic - please adjust! ###
     op.drop_table('usertalleres')
-    op.drop_table('tallerarticulos')
-    op.drop_table('pagotalleres')
-    op.drop_table('talleres')
     op.drop_table('comunicacion')
     op.drop_table('users')
+    op.drop_table('tallerarticulos')
+    op.drop_table('pagotalleres')
     op.drop_table('tipos')
+    op.drop_table('talleres')
     op.drop_table('roles')
     op.drop_table('pagos')
     op.drop_table('articulos')
