dea0bd75b513f6326bececc3c17e98028b9fd003
E-Vivanco <vivanco.elsa@gmail.com>
2023-05-09 07:09:29 +0000

    up files ok desde cloudinary + noticias y eventos renderizados en comunidad, pero se deben revisar ambos pues muestran la misma  info

--- src/front/js/pages/eventos.jsx
+++ src/front/js/pages/eventos.jsx
@@ -4,6 +4,9 @@ import Estructura1 from "./evento1.jsx";
 import Estructura2 from "./evento2.jsx";
 
 export const Eventos = () => {
+  const { store, actions } = useContext(Context);
+  console.log("que trae", store.contacto);
+  useEffect(() => actions.getComunicacion(), []);
   return (
     <div
       className="container-fluid position-relative d-inline-block p-2"
@@ -11,11 +14,24 @@ export const Eventos = () => {
     >
       <h1 className="text-center my-1">Eventos</h1>
       <hr className="hr" />
-      <div className="row row-cols-1 container-fluid m-0 p-2">
-        <Estructura1 />
-        {/* <br/>
+      {store.contacto?.length <= 0 ? (
+        <div className="row row-cols-1 container-fluid m-0 p-2">
+          <Estructura1 />
+          {/* <br/>
                 <Estructura2 /> */}
-      </div>
+        </div>
+      ) : (
+        store.contacto.Datos_de_Comunicacion.map((element) => (
+          <div className="row justify-content-center">
+            <Estructura1
+              titulo={element.titulo}
+              descripcion={element.descripcion}
+              created_at={element.created_at}
+              imagen={element.imagen}
+            />
+          </div>
+        ))
+      )}
     </div>
   );
 };
